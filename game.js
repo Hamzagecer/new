6d1dd27f235cf29eb8f2a13194f5b8ac81739ac1
Hamzagecer <etemmmete14@gmail.com>
2021-04-18 16:20:54 +0200

    first commit

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ while (pts.length < 254) {
 }
 pts.push(pts[0]);
 
-var lerp = (a, b, t) => a + (b-a) * (1-Math.cos(t * Math.PI))/2;
+var lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t * Math.PI)) / 2;
 
 var noise = x => {
     x = x * 0.01 % 254;
@@ -28,16 +28,88 @@ var forecolor = "#4a3f35";
 var linecolor = "2f2519";
 var linewidth = 5;
 var offset = -10;
+var yRatio = .2;
 var t = 0;
+var speed = 0;
+var playing = true;
+
+player = new function () {
+    this.x = c.width / 2;
+    this.y = 50;
+    this.truck = new Image();
+    this.truck.src = "./img/vf.png";
+    this.rot = 0;
+    this.ySpeed = 0;
+    this.rSpeed = 0;
+
+    this.draw = function () {
+
+
+        var p1 = (c.height * .9) - noise(this.x + t) * yRatio;
+        var p2 = (c.height * .9) - noise(this.x + t + 5) * yRatio;
+
+        var gnd = 0;
+        var offset = 38;
+
+        if (p1 - offset > this.y) {
+            this.ySpeed += 0.1;
+        } else {
+            this.ySpeed -= this.y - (p1 - offset);
+            this.y = p1 - offset;
+            gnd = 1;
+        }
+        if (!playing || gnd && Math.abs(this.rot) > Math.PI * 0.5) {
+            playing = false;
+            this.rSpeed = 5;
+            this.x -= speed * 5;
+
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.font = "32px Impact";
+            ctx.fillStyle = "white";
+            ctx.fillText("OYUN BITTI", c.width / 2, c.height / 3);
+
+        }
+
+
+
+
+
+        var angle = Math.atan2((p2 - offset) - this.y, (this.x + 5) - this.x);
+        if (gnd && playing) {
+            this.rot -= (this.rot - angle) * 0.5;
+            this.rSpeed = this.rSpeed - (angle - this.rot);
+        }
+
+
+        this.rot -= this.rSpeed * 0.1;
+
+        if (this.rot > Math.PI) this.rot = -Math.PI;
+        if (this.rot < -Math.PI) this.rot = -Math.PI;
+
+
+
+        this.y += this.ySpeed;
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rot);
+        ctx.drawImage(this.truck, -40, -40, 80, 80);
+        ctx.restore();
+
+    }
+}
 
 
 //draw
 function draw() {
-    t++;
+    speed -= (speed - 1) * 0.01;
+    t += 5 * speed;
 
 
-    ctx.fillStyle = bgcolor
-    ctx.fillRect(0, 0,c.width,c.height);
+    ctx.fillStyle = bgcolor;
+    ctx.fillRect(0, 0, c.width, c.height);
+    //player
+    player.draw();
 
     ctx.fillStyle = forecolor;
     ctx.strokeStyle = linecolor;
@@ -45,12 +117,14 @@ function draw() {
     ctx.beginPath();
     ctx.moveTo(offset, c.height - offset);
     for (let i = offset; i < c.width - offset; ++i) {
-        ctx.lineTo(i, (c.height * .9) - noise(i + t) * .4);
+        ctx.lineTo(i, (c.height * .9) - noise(i + t) * yRatio);
     }
     ctx.lineTo(c.width - offset, c.height - offset);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
+
+
 
     requestAnimationFrame(draw)
 }
